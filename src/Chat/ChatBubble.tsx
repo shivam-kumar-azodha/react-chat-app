afa8b7f431d0226b59d40ae81b62613e33078287
Shivam Kumar <shivam@azodha.com>
2024-07-07 17:36:39 +0530

    emoji  only message

--- src/Chat/ChatBubble.tsx
+++ src/Chat/ChatBubble.tsx
@@ -5,12 +5,20 @@ type ChatBubbleProps = {
   isSent: boolean;
 };
 
+const containsOnlyEmojis = (message: string) =>
+  message.match(/^(?:\p{Emoji_Presentation}|\p{Extended_Pictographic})+$/u);
+
 const ChatBubble: React.FC<ChatBubbleProps> = ({ message, isSent }) => {
+  const isOnlyEmoji = containsOnlyEmojis(message);
   return (
-    <div className={`flex ${isSent ? "justify-end" : "justify-start"} mb-4`}>
+    <div className={`flex ${isSent ? "justify-end" : "justify-start"} mb-2`}>
       <div
-        className={`max-w-xs md:max-w-md lg:max-w-lg px-4 py-2 rounded-lg text-white ${
-          isSent ? "bg-blue-500" : "bg-gray-300 text-black"
+        className={`max-w-xs md:max-w-md lg:max-w-lg rounded-lg text-white ${
+          isOnlyEmoji
+            ? "text-4xl" // Class to make emojis bigger
+            : isSent
+            ? "bg-blue-500 px-4 py-2"
+            : "bg-gray-300 text-black px-4 py-2"
         }`}
       >
         {message}
